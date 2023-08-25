ce2b7d28f234d27ce643ccb63c2859c964333845
Carlos Rodrigues <cerf@furg.br>
2023-08-25 12:08:45 -0300

    test: ensure Job updates correctly
    Signed-off-by: Carlos Rodrigues <cerf@furg.br>

--- tests/application/repositories/job_repository_test.rs
+++ tests/application/repositories/job_repository_test.rs
@@ -1,5 +1,6 @@
 #[cfg(test)]
 mod tests {
+    use chrono::Utc;
     use uuid::Uuid;
 
     use rust_video_encoder_service::application::repositories::{
@@ -32,5 +33,46 @@ mod tests {
         let get_job = job_repository.find_by_id(&job.clone().id()).await.unwrap();
         assert_eq!(get_job.id(), job.clone().id());
         assert_eq!(get_job.video_id(), job.clone().video_id());
+    }
+
+    #[tokio::test]
+    async fn test_job_repository_update() {
+        let db = Database::new_db_test().await;
+        let video = Video::new(
+            Uuid::new_v4().to_string(),
+            Uuid::new_v4().to_string(),
+            "any_path".to_string(),
+            chrono::Utc::now().naive_utc(),
+        );
+        let job = Job::new(
+            "any_path".to_string(),
+            "Pending".to_string(),
+            &video,
+            video.id().to_string(),
+        )
+        .unwrap();
+
+        let repository_db = VideoRepositoryImplementation::instance(&db);
+        let _ = repository_db.insert(&video).await;
+        let job_repository = JobRepositoryImplementation::instance(&db);
+        job_repository.insert(&job.clone()).await.unwrap();
+        let get_job = job_repository.find_by_id(&job.clone().id()).await.unwrap();
+        let update_job = Job::from(
+            get_job.id().to_string(),
+            get_job.output_bucket_path().to_string(),
+            "Complete".to_string(),
+            get_job.video(),
+            get_job.video_id().to_string(),
+            None,
+            get_job.created_at(),
+            Utc::now().naive_utc(),
+        )
+        .unwrap();
+        let job_update = job_repository.update(&update_job).await.unwrap();
+        let get_job = job_repository
+            .find_by_id(&job_update.clone().id())
+            .await
+            .unwrap();
+        assert_eq!(get_job.id(), job_update.clone().id());
     }
 }
